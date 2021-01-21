c74680296a224f4a38a2aae86256aedaf945ca06
stephanie <stephieplusthree@gmail.com>
2021-01-20 21:11:36 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+const App = () => {
+
+  const [profileName, setProfileName] = useState("");
+  const [profileCell, setProfileCell] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [profileEmail, setProfileEmail] = useState("");
+
+  const profileData = async () => {
+    try {
+      const res = await axios.get("https://randomuser.me/api");
+      setProfileCell(res.data.results[0].cell);
+      setProfileEmail(res.data.results[0].email);
+      setProfileImage(res.data.results[0].picture.large);
+      setProfileName(
+        `${res.data.results[0].name.first} ${res.data.results[0].name.last}`
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    profileData();
+  }, []);
+
+  return ( 
+    <div>
+      <button onClick={() => profileData()}>New Person</button>
+      <div className="card">
+        <img src={profileImage} style={{ width: "100%" }} />
+        <h1>{profileName}</h1>
+        <p className="title">{profileEmail}</p>
+        <p>{profileCell}</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <button>Contact</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
